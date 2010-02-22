0f370a6f185002fe85bce7317ff69adcceb8d494
Andy Lester <andy@petdance.com>
2010-02-22 00:27:04 -0600

    releasing 0.99

--- src/version.h
+++ src/version.h
@@ -3,12 +3,6 @@
   (c) 2007-2009 (W3C) MIT, ERCIM, Keio University
   See tidyp.h for the copyright notice.
 
-  CVS Info :
-
-    $Author: arnaud02 $ 
-    $Date: 2009/03/25 21:37:11 $ 
-    $Revision: 1.46 $ 
-
 */
 
-static const char TY_(version)[] = "0.90";
+static const char TY_(version)[] = "0.99";
