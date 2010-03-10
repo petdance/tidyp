a16cf5d01a7d9ef38daf64e9f08ed341d3389c9e
Andy Lester <andy@petdance.com>
2010-03-10 00:36:49 -0600

    Moved compiler.h into platform.h

--- include/platform.h
+++ include/platform.h
@@ -548,7 +548,46 @@ typedef const tmbchar* ctmbstr; /* Ditto, but const */
 #define TIDY_CALL
 #endif
 
-#include "compiler.h"
+#if defined(__GNUC__) || defined(__INTEL_COMPILER)
+# define ARG_UNUSED(x) x __attribute__((unused))
+#else
+# define ARG_UNUSED(x) x
+#endif
+
+#define NOTNULL(x)                  /*@notnull@*/ x
+    /* The pointer passed may not be NULL */
+
+#define NULLOK(x)                   /*@null@*/ x
+    /* The pointer passed may be NULL */
+
+#define ARGIN(x)                    /*@in@*/ /*@notnull@*/ x
+#define ARGIN_NULLOK(x)             /*@in@*/ /*@null@*/ x
+    /* The pointer target must be completely defined before being passed */
+    /* to the function. */
+
+#define ARGOUT(x)                   /*@out@*/ /*@notnull@*/ x
+#define ARGOUT_NULLOK(x)            /*@out@*/ /*@null@*/ x
+    /* The pointer target will be defined by the function */
+
+#define ARGMOD(x)                   /*@in@*/ /*@notnull@*/ x
+#define ARGMOD_NULLOK(x)            /*@in@*/ /*@null@*/ x
+    /* The pointer target must be completely defined before being passed, */
+    /* and MAY be modified by the function. */
+
+#define FUNC_MODIFIES(x)            /*@modifies x@*/
+    /* Never applied by a human, only by the headerizer. */
+
+#define ARGFREE(x)                          /*@only@*/ /*@out@*/ /*@null@*/ x
+    /* From the Splint manual: The parameter to free() must reference */
+    /* an unshared object.  Since the parameter is declared using "only", */
+    /* the caller may not use the referenced object after the call, and */
+    /* may not pass in a reference to a shared object.  There is nothing */
+    /* special about malloc and free --  their behavior can be described */
+    /* entirely in terms of the provided annotations. */
+
+#define __attribute__warn_unused_result__ __attribute__((__warn_unused_result__))
+#define TIDY_IGNORABLE_RESULT
+#define TIDY_WARN_UNUSED_RESULT   __attribute__warn_unused_result__
 
 /* HAS_VSNPRINTF triggers the use of "vsnprintf", which is safe related to
    buffer overflow. Therefore, we make it the default unless HAS_VSNPRINTF
